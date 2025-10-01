be047f88ea6b19d4c11d8f4fac8151063a2b26f1
Juan Antonio <juananthonio23@gmail.com>
2025-10-02 00:55:28 +0700

    Refactor AboutPage layout and styles for improved presentation and consistency

--- app/tentang-kami/page.tsx
+++ app/tentang-kami/page.tsx
@@ -204,10 +204,10 @@ const AboutPage = () => {
         </div>
       </section>
 
-      <section className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-16'>
+      <section className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
         {/* Why Choose Us Section */}
         <div className='text-center'>
-          <h3 className='text-3xl font-semibold text-primary mb-8 text-balance'>
+          <h3 className='text-3xl font-semibold text-primary mb-10 text-balance'>
             Mengapa Memilih Kami?
           </h3>
           <ul className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
@@ -241,7 +241,9 @@ const AboutPage = () => {
                 key={item.title}
                 className='flex flex-col items-center p-4 py-8 bg-third rounded-xl transition-all duration-300 group'
               >
-                {<item.icon size={42} className='text-primary mb-4' />}
+                <div className='bg-primary p-3 rounded-full mb-4 flex items-center justify-center transition-colors duration-300'>
+                  {<item.icon size={32} className='text-white' />}
+                </div>
                 <h5 className='text-xl font-semibold text-primary mb-2'>
                   {item.title}
                 </h5>
@@ -254,12 +256,12 @@ const AboutPage = () => {
         </div>
       </section>
 
-      <section>
-        <div className='p-8 py-12 flex flex-col items-center text-center bg-[url("/img/img2.webp")] bg-cover bg-center backdrop:brightness-50'>
+      <section className='mt-8'>
+        <div className='p-8 py-12 flex flex-col items-center text-center relative before:bg-[url("/img/img3.webp")] before:bg-cover before:bg-center before:contents-[""] before:absolute before:inset-0 before:brightness-50 before:-z-10'>
           <h3 className='text-3xl font-bold text-white mb-4'>
             Siap Bermitra dengan Kami?
           </h3>
-          <p className='text-lg text-white/90 mb-8 max-w-2xl'>
+          <p className='text-lg text-gray-300 mb-8 max-w-2xl'>
             Hubungi kami untuk konsultasi atau penawaran solusi alat berat dan
             spare parts.
           </p>
